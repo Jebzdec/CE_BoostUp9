92773aa0de8f1274e4fd74463faa156b80fad84b
Jebz <64010818@kmitl.ac.th>
2021-08-23 22:17:25 +0700

    -

---
+++ 132.cpp
@@ -0,0 +1,8 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int a,b;
+    cin>>a>>b;
+    int i,j;
+    while(i+j)
+}
--- 48.cpp
+++ 48.cpp
@@ -2,40 +2,16 @@
 #include<string.h>
 #include<math.h>
 using namespace std;
-char p[100]={};
-int func2(int a,int k){
-    if(a>0){
-    p[k]=(a%2)+48;
-    func2(a/2,k+1);
-}}
-int func(char a[],int n,int k){
-    if(n>0) return func(a,n-1,k+1)+(int(a[n])-48)*pow(2,k);
-    else return 0;
+char* process(char x[]){
+    for(int i=0;i<strlen(x);i++) (x[i]=='0')? x[i]='1':x[i]='0';
+    //cout<<x<<endl;
+    return x;
 }
 int main(){
-char a[100],b[100],c;
-cin>>a>>c>>b;
-int x=func(a,strlen(a)-1,0);
-int y=func(b,strlen(b)-1,0);
-if(a[0]=='1') x*=-1;
-if(b[0]=='1') y*=-1;
-if(c=='+'){
-    if(x+y>127 or x+y<-127){
-        cout<<"Out of Memory";
-        goto finish;
-    }
-    (x+y<0)?cout<<1:cout<<0;
-    func2(abs(x+y),0);
-}
-if(c=='-'){
-    if(x-y>127 or x-y<-127){
-        cout<<"Out of Memory";
-        goto finish;
-    }
-    (x-y<0)?cout<<1:cout<<0;
-    func2(abs(x-y),0);
-}
-
-for(int i=strlen(p)-1;i>=0;i--) cout<<p[i];
-finish:;
+    char a[9],b[9],c;
+    cin>>a>>c>>b;
+    if(a[0]=='1') strcpy(a,process(a));
+    if(b[0]=='1') strcpy(b,process(b));
+    if(b[0]=='-') strcpy(b,process(b));
+    cout<<a<<endl<<b;
 }
